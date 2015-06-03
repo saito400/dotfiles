ca586f778fcec9eb5aa16076f1ddf01c6f7c18c6
saito400 <ken.s.530000@gmail.com>
2015-06-03 09:03:14 +0900

    add ensime setting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -82,6 +82,19 @@
 (require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 
+(setq ensime-sem-high-faces
+ '((var . (:foreground "#ff2222"))
+   (val . (:foreground "#dddddd"))
+   (varField . (:foreground "#ff3333"))
+   (valField . (:foreground "#dddddd"))
+   (functionCall . (:foreground "#84BEE3"))
+   (param . (:foreground "#ffffff"))
+   (class . font-lock-type-face)
+   (trait . (:foreground "#084EA8"))
+   (object . (:foreground "#026DF7"))
+   (package . font-lock-preprocessor-face)))
+
+
 ;; helm-mini
 (progn
   (require 'helm-ls-git)
