bfbdb27f8b1624d921547835aef81d5ce8339663
saito400 <ken.s.530000@gmail.com>
2015-08-07 19:41:35 +0900

    update melpa url

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,7 +30,7 @@
 ;; for package.el
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
